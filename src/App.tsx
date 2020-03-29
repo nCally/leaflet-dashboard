ad02acd9ffaaa15f90f7cd6e4d16a152c6ef947a
nCally <cartrf@live.com>
2020-03-29 20:18:47 +0100

    marker moves

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Map as LeafletMap, TileLayer, Marker } from "react-leaflet";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [position, changePosition] = useState({
+        lat:6.458726,
+        lng:7.527588
+    })
+
+    useEffect(function(){
+        /* const interval = setInterval(function(){
+            console.log('change')
+            changePosition({...position, lng:position.lng - 0.00009})
+        }, 1000)
+
+        return () => clearInterval(interval) */
+    })
+
+    return (
+        <div>
+            <LeafletMap center={[6.458726,7.527588]} zoom={15} attributionControl={true}>
+                <TileLayer id="mapbox/streets-v11"
+                url={`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibmNhbGx5IiwiYSI6ImNrN3Vud3N5bzE1cWwzbXFyc3lpaHlldWoifQ.-HWevPOSU8PIR6HSP4R8Lw`} />
+                <Marker position={[position.lat,position.lng]} />
+            </LeafletMap>
+        </div>
+    );
 }
 
 export default App;
